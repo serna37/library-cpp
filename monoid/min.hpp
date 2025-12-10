ca56b8af3acd177b6c7f811a876fdbc80a8f4a1f
さーな <37serna37serna37serna@gmail.com>
2025-12-10 21:49:49 +0900

    add monoid min

---
+++ monoid/min.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "template/inf.hpp"
+
+/**
+ * 最小値モノイド
+ */
+template <typename T> struct Mmin {
+    using value_type = T;
+    static constexpr T e = inf<T>;
+    static constexpr T op(const T &x, const T &y) noexcept {
+        return min(x, y);
+    }
+};
