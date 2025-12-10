9b11547e302b845b37973432069028e57960d739
さーな <37serna37serna37serna@gmail.com>
2025-12-10 21:36:59 +0900

    add template

---
+++ template/template.cpp
@@ -0,0 +1,13 @@
+/** ฅ^>ω<^ฅ author: serna37 */
+#ifdef LOCAL
+#include "template/debug.hpp"
+#else
+#define debug(...)
+#endif
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout << fixed << setprecision(20);
+}
