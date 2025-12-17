dee3fa409e7b77c978040ffbf1badbe15adb29c2
さーな <37serna37serna37serna@gmail.com>
2025-12-17 23:43:16 +0900

    ライブラリのincludeをある程度常駐させる

--- template/debug.hpp
+++ template/debug.hpp
@@ -1,5 +1,4 @@
 #pragma once
-
 /**
  * @brief debug出力
  */
--- template/template.cpp
+++ template/template.cpp
@@ -1,16 +1,20 @@
 /**
  * @brief ฅ^>ω<^ฅ
- * @author: serna37
+ * @author serna37
  */
 #ifdef LOCAL
-#include <template/debug.hpp>
+#include "template/debug.hpp"
 #else
 #define debug(...)
 #endif
 #include <bits/stdc++.h>
 using namespace std;
+#include "library/io.hpp"
+#include "library/const.hpp"
+#include "library/util.hpp"
+//#include "library/string.hpp"
+//#include "library/geo.hpp"
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(0)->sync_with_stdio(0);
     cout << fixed << setprecision(20);
 }
