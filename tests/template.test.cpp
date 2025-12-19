43e160e2cb145e50d621b02b21f8d2f067eaf4d6
さーな <95469388+serna37@users.noreply.github.com>
2025-12-19 17:30:54 +0900

    おためし

--- tests/template.test.cpp
+++ tests/template.test.cpp
@@ -1,5 +1,7 @@
 #define PROBLEM https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_1_A
-#include "template/template.hpp"
+//#include "template/template.hpp"
+#include <bits/stdc++.h>
+using namespace std;
 signed main() {
     // TODO assertしまくる
     cout << "Hello World" << endl;
