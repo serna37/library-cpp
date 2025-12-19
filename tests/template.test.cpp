b8b199783a3a72de9a85e8f5ad9d7b6d6b2dba71
さーな <95469388+serna37@users.noreply.github.com>
2025-12-19 17:16:56 +0900

    templateのテストを追加

---
+++ tests/template.test.cpp
@@ -0,0 +1,6 @@
+#define PROBLEM https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_1_A
+#include "template/template.hpp"
+signed main() {
+    // TODO assertしまくる
+    cout << "Hello World" << endl;
+}
