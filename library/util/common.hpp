e194938133e96d1f6bc2f8bb566b71e52a0aa43a
さーな <95469388+serna37@users.noreply.github.com>
2025-12-19 17:02:44 +0900

    緩和をリファクタ

--- library/util/common.hpp
+++ library/util/common.hpp
@@ -2,25 +2,9 @@
 #define int long long
 #define all(x) begin(x), end(x)
 #define rall(x) rbegin(x), rend(x)
-/**
- * 緩和 min
- * chmin(i, j); iをより小さいほうにする
- */
 auto chmin = []<class T>(T &i, T &j) {
-    if (i > j) {
-        i = j;
-        return true;
-    }
-    return false;
+    return i > j && (i = j, true);
 };
-/**
- * 緩和 max
- * chmax(i, j); iをより大きいほうにする
- */
 auto chmax = []<class T>(T &i, T &j) {
-    if (i < j) {
-        i = j;
-        return true;
-    }
-    return false;
+    return i < j && (i = j, true);
 };
