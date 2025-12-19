27dd4f50ff4381907012ff2a4e13b1c82fbd9883
さーな <95469388+serna37@users.noreply.github.com>
2025-12-19 09:36:30 +0900

    infinityについてlong long側定数をベーシックな命名にする

--- library/const.hpp
+++ library/const.hpp
@@ -6,13 +6,13 @@
 template <class T> constexpr T inf = 0;
 template <> constexpr int inf<int> = 1e9;
 template <> constexpr long long inf<long long> = 1e18;
-const int INF = 1e9;
-const long long INFL = 1e18;
+const int INF_INT = 1e9; // 基本long longを使用
+const long long INF = 1e18;
 const long long MOD = 998244353;
 const long long MOD7 = 1000000007;
 const long double EPS = 1e-14;
-const int MAX = 2e6;
-const long long MAXL = 2e6;
+const int MAX_INT = 2e6;  // 基本long longを使用
+const long long MAX = 2e6;
 const double PI = acos(-1);
 // 下右上左 DRUL
 const vector<int> dx = {0, 1, 0, -1};
