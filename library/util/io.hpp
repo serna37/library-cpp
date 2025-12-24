f901a63092226c43053b7fba8542ecf79b2d016b
さーな <37serna37serna37serna@gmail.com>
2025-12-24 15:35:11 +0900

    参照のみの引数にconstをつけて、一時的な値でも入れられるようにする

--- library/util/io.hpp
+++ library/util/io.hpp
@@ -14,18 +14,18 @@ template <typename T> istream &operator>>(istream &s, vector<T> &v) {
     for (auto &&e : v) s >> e;
     return s;
 }
-template <typename T> ostream &operator<<(ostream &s, vector<T> &v) {
+template <typename T> ostream &operator<<(ostream &s, const vector<T> &v) {
     int i = 0;
     for (auto &&e : v) s << (i++ ? " " : "") << e;
     return s;
 }
-template <typename T> void print(vector<vector<T>> &G) {
+template <typename T> void print(const vector<vector<T>> &G) {
     for (auto &&v : G) cout << v << endl;
 }
-template <typename T, typename U> void print(pair<T, U> &p) {
+template <typename T, typename U> void print(const pair<T, U> &p) {
     cout << p.first << " " << p.second << endl;
 }
-template <typename T> void print(T &v) {
+template <typename T> void print(const T &v) {
     cout << v << endl;
 }
 void yn(bool o) {
