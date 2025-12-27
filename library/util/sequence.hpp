52c9689b219c84e294b6e65102e2fdd8bee0700b
さーな <37serna37serna37serna@gmail.com>
2025-12-27 20:43:02 +0900

    累積和系を追加

--- library/util/sequence.hpp
+++ library/util/sequence.hpp
@@ -110,5 +110,90 @@ class Sequence {
         }
         return inv;
     }
+    // 累積和 S[i]=Σ(0,i-1)  Σ[l,r)=S[r]-S[l]
+    template <typename T> vector<long long> zeta(vector<T> &A) {
+        int N = A.size();
+        vector<long long> S(N + 1);
+        for (int i = 0; i < N; ++i) S[i + 1] = S[i] + A[i];
+        return S;
+    }
+    // 反転累積和 S[i]=Aiから右全部  Σ(0,i-1)+Σ(i,last)=S[i]+R[i]
+    template <typename T> vector<long long> zeta_rev(vector<T> &A) {
+        int N = A.size();
+        vector<long long> R(N + 1);
+        for (int i = N - 1; i >= 0; --i) R[i] = R[i + 1] + A[i];
+        return R;
+    }
+    // 二項差での累積和 S[i]=Σ(0,i-1)  Σ[l,r)=S[r]-S[l]
+    template <typename T> vector<long long> zeta_diff(vector<T> &A) {
+        int N = A.size();
+        vector<long long> S(N + 1);
+        for (int i = 0; i < N; ++i) {
+            S[i + 1] = S[i];
+            if (i & 1) S[i + 1] += abs(A[i] - A[i - 1]);
+        }
+        return S;
+    }
+    // 二項差での反転累積和 S[i]=Aiから右全部  Σ(0,i-1)+Σ(i,last)=S[i]+R[i]
+    template <typename T> vector<long long> zeta_rev_diff(vector<T> &A) {
+        int N = A.size();
+        vector<long long> R(N + 1);
+        for (int i = N - 1; i >= 0; --i) {
+            R[i] = R[i + 1];
+            if (i & 1) R[i] += abs((i + 1 < N ? A[i + 1] : 0) - A[i]);
+        }
+        return R;
+    }
+    /**
+     * 2次元平面での累積和
+     * 包除: (s,t)~(x,y) = S[y][x]-S[y][s]-S[t][x]+S[t][s]
+     */
+    template <typename T> vector<vector<long long>> zeta_2D(vector<T> &G) {
+        int H = G.size(), W = G[0].size();
+        vector<vector<long long>> S(H + 1, vector<long long>(W + 1));
+        for (int i = 0; i < H; ++i) { // 横向き
+            for (int j = 0; j < W; ++j) {
+                S[i + 1][j + 1] = S[i + 1][j] + G[i][j];
+            }
+        }
+        for (int i = 0; i < H; ++i) { // 縦向き
+            for (int j = 0; j < W; ++j) {
+                S[i + 1][j + 1] += S[i][j + 1];
+            }
+        }
+        return S;
+    }
+    /**
+     * 3次元累積和
+     * ex) Lx Ly Lzは0-indexed Rx Ry Rzは1-indexed
+     *   S[Rxyz] - S[Lxyz]
+     *          = S[Rx][Ry][Rz] - S[Lx][Ry][Rz] - S[Rx][Ly][Rz] -
+     *            S[Rx][Ry][Lz] + S[Lx][Ly][Rz] + S[Lx][Ry][Lz] +
+     *            S[Rx][Ly][Lz] - S[Lx][Ly][Lz];
+     */
+    template <typename T>
+    vector<vector<vector<long long>>> zeta_3D(vector<vector<vector<T>>> &A) {
+        vector<vector<vector<long long>>> S;
+        int szx = A.size(), szy = A[0].size(), szz = A[0][0].size();
+        S.resize(szx + 1, vector<vector<long long>>(
+                              szy + 1, vector<long long>(szz + 1, 0)));
+        for (int x = 1; x <= szx; ++x) {
+            for (int y = 1; y <= szy; ++y) {
+                for (int z = 1; z <= szz; ++z) {
+                    S[x][y][z] = A[x - 1][y - 1][z - 1] + S[x - 1][y][z] +
+                                 S[x][y - 1][z] + S[x][y][z - 1] -
+                                 S[x - 1][y - 1][z] - S[x - 1][y][z - 1] -
+                                 S[x][y - 1][z - 1] + S[x - 1][y - 1][z - 1];
+                }
+            }
+        }
+        return S;
+    }
+    // 各項の差の数列 メビウス変換
+    template <typename T> vector<long long> moebius(vector<T> &A) {
+        int N = A.size();
+        vector<long long> D(N - 1);
+        for (int i = 0; i < N; ++i) D[i] = A[i + 1] - A[i];
+        return D;
+    }
 };
-// TODO 累積和系
