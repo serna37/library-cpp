4b54b750263fa31b59e4cd5293b0b5858753c551
さーな <37serna37serna37serna@gmail.com>
2025-12-27 21:03:10 +0900

    ラムダの黒魔術マクロを追加

--- library/def/common.hpp
+++ library/def/common.hpp
@@ -6,5 +6,8 @@
 #define int long long
 #define all(x) begin(x), end(x)
 #define rall(x) rbegin(x), rend(x)
+#define $(idx) (std::get<(idx)>(std::forward_as_tuple(_args...)))
+// lambda($(0) + $(1))とできる
+#define lambda(...) ([&](auto &&..._args) { return (__VA_ARGS__); })
 auto chmin = []<class T>(T &i, const T &j) { return i > j && (i = j, true); };
 auto chmax = []<class T>(T &i, const T &j) { return i < j && (i = j, true); };
