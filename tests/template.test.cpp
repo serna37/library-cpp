dfa77e91421032d5f6fe55ee1b1a8739768bf160
さーな <37serna37serna37serna@gmail.com>
2026-01-01 16:47:37 +0900

    テスト追加: 数の性質

--- tests/template.test.cpp
+++ tests/template.test.cpp
@@ -8,7 +8,34 @@ library/util/xxx.hppのユニットテスト
 */
 // ===== library/util/number.hpp =====
 void test_number() {
-    // TODO test
+    assert(lib::num.div_ceil(10, 2) == 5);
+    assert(lib::num.div_ceil(11, 2) == 6);
+    assert(lib::num.div_floor(10, 2) == 5);
+    assert(lib::num.div_floor(11, 2) == 5);
+    assert(lib::num.factorial(0) == 1);
+    assert(lib::num.factorial(1) == 1);
+    assert(lib::num.factorial(2) == 2);
+    assert(lib::num.factorial(3) == 6);
+    assert(lib::num.combination(0, 0) == 1);
+    assert(lib::num.combination(1, 0) == 1);
+    assert(lib::num.combination(1, 1) == 1);
+    assert(lib::num.combination(2, 0) == 1);
+    assert(lib::num.combination(3, 2) == 3);
+    assert(lib::num.digit_sum(1234) == 10);
+    assert(lib::num.base_convert("1001", 2, 10) == "9");
+    vector<int> val_divisors = lib::num.divisors(12);
+    lib::seq.sort_asc(val_divisors);
+    vector<int> exp_divisors = {1, 2, 3, 4, 6, 12};
+    assert(val_divisors == exp_divisors);
+    assert(lib::num.prime_test(57) == false); // Grothendieck
+    assert(lib::num.prime_test(97) == true);
+    map<int, int> val_prime_fact = lib::num.prime_fact(100);
+    map<int, int> exp_prime_fact = {{2, 2}, {5, 2}};
+    assert(val_prime_fact == exp_prime_fact);
+    assert(lib::num.modpow(2, 10, MOD) == 1024);
+    assert(lib::num.modinv(2, MOD) == 499122177);
+    assert(lib::num.modfact(10, MOD) == 3628800);
+    assert(lib::num.mod_combination(10, 5, MOD) == 252);
 }
 // ===== library/util/string.hpp =====
 void test_string() {
