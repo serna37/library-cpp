852da21e7531c8a8d807429efb17f2c692f0d342
さーな <37serna37serna37serna@gmail.com>
2026-01-02 11:33:30 +0900

    テスト追加: Union Find

---
+++ tests/struct.union_find.test.cpp
@@ -0,0 +1,22 @@
+#define PROBLEM                                                                \
+    "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_1_A"
+#include "template/template.hpp"
+#include "library/struct/union_find.hpp"
+/**
+ * @brief 🍪🧸🐾
+ */
+void solve() {
+    int N, Q;
+    cin >> N >> Q;
+    UnionFind uf(N);
+    while (Q--) {
+        int com, x, y;
+        cin >> com >> x >> y;
+        if (com == 0) {
+            uf.unite(x, y);
+        }
+        if (com == 1) {
+            print(uf.find(x) == uf.find(y) ? 1 : 0);
+        }
+    }
+}
