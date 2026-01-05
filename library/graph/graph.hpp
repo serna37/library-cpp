f533ebc247cddbbcf4c2fa59c13f046ec8691676
さーな <37serna37serna37serna@gmail.com>
2026-01-05 21:23:57 +0900

    add cycle detect

--- library/graph/graph.hpp
+++ library/graph/graph.hpp
@@ -136,7 +136,51 @@ struct Graph {
         }
         return {dis, negativeCycle};
     }
-    // TODO サイクル検出
+    /**
+     * 閉路検出
+     * @return サイクルな辺の集合
+     */
+    vector<Edge> cycle_detect(bool directed) {
+        vector<bool> seen(N), finished(N);
+        vector<Edge> history;
+        auto dfs = [&](auto &f, int v, const Edge &e) -> int {
+            seen[v] = true;
+            history.push_back(e);
+            for (const auto &ne : G[v]) {
+                auto [from, to, id, cost] = ne;
+                if (!directed and to == e.from) continue;
+                if (finished[to]) continue;
+                if (seen[to] and !finished[to]) {
+                    history.push_back(ne);
+                    return to;
+                }
+                int pos = f(f, to, ne);
+                if (pos != -1) return pos;
+            }
+            finished[v] = true;
+            history.pop_back();
+            return -1;
+        };
+        auto restruct = [&](int pos) -> vector<Edge> {
+            vector<Edge> cycle;
+            while (!history.empty()) {
+                const Edge e = history.back();
+                cycle.push_back(e);
+                history.pop_back();
+                if (e.from == pos) break;
+            }
+            reverse(cycle.begin(), cycle.end());
+            return cycle;
+        };
+        int pos = -1;
+        for (int v = 0; v < N and pos == -1; ++v) {
+            if (seen[v]) continue;
+            history.clear();
+            pos = dfs(dfs, v, Edge({-1, -1, -1, -1}));
+            if (pos != -1) return restruct(pos);
+        }
+        return vector<Edge>();
+    }
     /**
      * DAGのトポロジカルソート
      */
