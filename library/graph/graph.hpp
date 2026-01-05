e0029c4e0ae0c9e5f501771d2b8e0cdc1e402ec0
さーな <37serna37serna37serna@gmail.com>
2026-01-05 21:26:24 +0900

    add strongly connected component

--- library/graph/graph.hpp
+++ library/graph/graph.hpp
@@ -7,6 +7,42 @@ struct Graph {
   private:
     int N;
     vector<vector<Edge>> G;
+    /**
+     * 強連結成分分解 O(V+E)
+     * @return sccのid、scc集合
+     */
+    pair<int, vector<int>> strongly_connected_component() {
+        int C = 0, now = 0;
+        vector<int> comp(N), low(N), ord(N, -1), pth;
+        auto dfs = [&](auto &f, int v) {
+            low[v] = ord[v] = now++;
+            pth.emplace_back(v);
+            for (auto &&[from, to, cost] : G[v]) {
+                if (ord[to] == -1) {
+                    f(f, to);
+                    low[v] = min(low[v], low[to]);
+                } else {
+                    low[v] = min(low[v], ord[to]);
+                }
+            }
+            if (low[v] == ord[v]) {
+                while (1) {
+                    int u = pth.back();
+                    pth.pop_back();
+                    ord[u] = N, comp[u] = C;
+                    if (u == v) break;
+                }
+                ++C;
+            }
+        };
+        for (int v = 0; v < N; ++v) {
+            if (ord[v] == -1) dfs(dfs, v);
+        }
+        for (int v = 0; v < N; ++v) {
+            comp[v] = C - 1 - comp[v];
+        }
+        return {C, comp};
+    }
 
   public:
     Graph(int N) : N(N), G(N) {
@@ -242,7 +278,37 @@ struct Graph {
         }
         return res;
     }
-    // TODO scc
+    /**
+     * 強連結成分分解
+     * @return scc集合のリスト。トポソ順
+     */
+    vector<vector<int>> scc_vertex_list() {
+        auto [cnt, ids] = strongly_connected_component(G);
+        vector<int> c(cnt);
+        vector<vector<int>> g(cnt);
+        for (auto &&v : ids) ++c[v];
+        for (int i = 0; i < cnt; ++i) g[i].reserve(c[i]);
+        for (int i = 0; i < N; ++i) g[ids[i]].push_back(i);
+        return g;
+    }
+    /**
+     * 強連結成分分解
+     * @return 強連結成分なグラフ構造のリスト。成分同士の情報ロストに注意
+     */
+    vector<Graph> scc() {
+        vector<vector<int>> components = scc_vertex_list(G);
+        vector<Graph> res;
+        for (auto &&comp : components) {
+            Graph sub(N);
+            for (auto &&v : comp) {
+                for (auto &&[from, to, cost] : G[v]) {
+                    sub.add(from, to, cost);
+                }
+            }
+            res.push_back(sub);
+        }
+        return res;
+    }
     // TODO クラスカル
     // TODO ドキュメント
 };
