4ad989bce781d2fff7c33faa9a4763710e4c9a72
さーな <37serna37serna37serna@gmail.com>
2026-01-06 20:10:23 +0900

    sccのテスト追加

---
+++ tests/graph/strongly_connected_components.test.cpp
@@ -0,0 +1,21 @@
+#define PROBLEM                                                                \
+    "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_3_C"
+#include "template/template.hpp"
+#include "library/graph/graph.hpp"
+/**
+ * @brief グラフ - 強連結成分分解のテスト
+ */
+void solve() {
+    int V, E;
+    cin >> V >> E;
+    Graph G(V);
+    G.read(E, 0, false, true);
+    auto [groups, ids] = G.scc();
+    int Q;
+    cin >> Q;
+    while (Q--) {
+        int u, v;
+        cin >> u >> v;
+        print(ids[u] == ids[v] ? "1" : "0");
+    }
+}
