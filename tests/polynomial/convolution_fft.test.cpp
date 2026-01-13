384ecfd9ce4626c69269c7eac05fd140a57ee431
さーな <95469388+serna37@users.noreply.github.com>
2026-01-13 09:23:31 +0900

    テスト追加

---
+++ tests/polynomial/convolution_fft.test.cpp
@@ -0,0 +1,61 @@
+#define PROBLEM                                                                \
+    "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_1_A"
+#include "template/template.hpp"
+#include "library/polynomial/convolution_fft.hpp"
+void run_unit_tests() {
+    // ケース1: 基本的な畳み込み (1 + 2x + 3x^2) * (4 + 5x + 6x^2)
+    {
+        vector<int> a = {1, 2, 3};
+        vector<int> b = {4, 5, 6};
+        vector<long long> expected = {4, 13, 28, 27, 18};
+        vector<long long> res = convolution_fft(a, b);
+        
+        assert(res.size() == expected.size());
+        for (int i = 0; i < (int)res.size(); i++) {
+            assert(res[i] == expected[i]);
+        }
+    }
+
+    // ケース2: 長さが異なる数列の畳み込み
+    {
+        vector<int> a = {1, 1, 1, 1};
+        vector<int> b = {1, 2};
+        // (1+x+x^2+x^3)*(1+2x) = 1 + 3x + 3x^2 + 3x^3 + 2x^4
+        vector<long long> expected = {1, 3, 3, 3, 2};
+        vector<long long> res = convolution_fft(a, b);
+        
+        assert(res.size() == expected.size());
+        for (int i = 0; i < (int)res.size(); i++) {
+            assert(res[i] == expected[i]);
+        }
+    }
+
+    // ケース3: 0を含むケース
+    {
+        vector<int> a = {0, 5, 0};
+        vector<int> b = {0, 2};
+        // (5x) * (2x) = 10x^2
+        vector<long long> expected = {0, 0, 10, 0};
+        vector<long long> res = convolution_fft(a, b);
+        
+        assert(res.size() == expected.size());
+        for (int i = 0; i < (int)res.size(); i++) {
+            assert(res[i] == expected[i]);
+        }
+    }
+
+    // ケース4: 大きな値の精度確認 (10^6 * 10^6 = 10^12)
+    {
+        vector<long long> a = {1000000};
+        vector<long long> b = {1000000};
+        vector<long long> res = convolution_fft(a, b);
+        assert(res[0] == 1000000000000LL);
+    }
+}
+/**
+ * @brief 畳み込みFFTのテスト
+ */
+void solve() {
+    print("Hello World");
+    run_unit_tests();
+}
---
+++ tests/sequence/difference_sequence.test.cpp
@@ -0,0 +1,47 @@
+#define PROBLEM                                                                \
+    "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_1_A"
+#include "template/template.hpp"
+#include "library/sequence/difference_sequence.hpp"
+void run_difference_sequence_tests() {
+    // ケース1: 基本的な数列
+    {
+        vector<int> A = {1, 3, 6, 10};
+        // 期待される結果:
+        // D[0] = A[1] - A[0] = 2
+        // D[1] = A[2] - A[1] = 3
+        // D[2] = A[3] - A[2] = 4
+        vector<long long> expected = {2, 3, 4};
+        
+        auto D = difference_sequence(A);
+
+        assert(D.size() == 3);
+        for (int i = 0; i < (int)D.size(); i++) {
+            assert(D[i] == expected[i]);
+        }
+    }
+
+    // ケース2: 値が減少する場合
+    {
+        vector<int> A = {10, 7, 5, 5};
+        vector<long long> expected = {-3, -2, 0};
+        auto D = difference_sequence(A);
+        for (int i = 0; i < 3; i++) {
+            assert(D[i] == expected[i]);
+        }
+    }
+
+    // ケース3: 要素が2つの場合 (最小サイズ)
+    {
+        vector<int> A = {1, 100};
+        auto D = difference_sequence(A);
+        assert(D.size() == 1);
+        assert(D[0] == 99);
+    }
+}
+/**
+ * @brief 階差数列のテスト
+ */
+void solve() {
+    print("Hello World");
+    run_difference_sequence_tests();
+}
