b51d0e2aa6851b3989d7761f61306a859cd9d364
さーな <95469388+serna37@users.noreply.github.com>
2026-01-19 12:38:39 +0900

    オイラーのφ関数を追加

---
+++ library/number/euler_phi.hpp
@@ -0,0 +1,12 @@
+#pragma once
+template <typename T> T euler_phi(T n) {
+    T ret = n;
+    for (T i = 2; i * i <= n; ++i) {
+        if (n % i == 0) {
+            ret -= ret / i;
+            while (n % i == 0) n /= i;
+        }
+    }
+    if (n > 1) ret -= ret / n;
+    return ret;
+}
