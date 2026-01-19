d3918ba0b05442aa1ede0dd23dad5ab629bd6ecb
さーな <95469388+serna37@users.noreply.github.com>
2026-01-19 13:23:18 +0900

    第一種スターリング数を追加

--- library/polynomial/fps/stirling_first_number.hpp
+++ library/polynomial/fps/stirling_first_number.hpp
@@ -1,2 +1,10 @@
 #pragma once
-// TODO
+#include "library/polynomial/fps/formal_power_series.hpp"
+template <template <typename> class FPS, typename Mint>
+FPS<Mint> stirling_first_number(int N) {
+    if (N <= 0) return {Mint(1)};
+    auto f = stirling_first_number<FPS, Mint>(N >> 1);
+    f *= f.taylor_shift(-(N >> 1));
+    if (N & 1) f = (f << 1) - f * (N - 1);
+    return f;
+}
