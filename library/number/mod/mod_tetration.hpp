30643373eb264d22bc3220c37dbba1926c0b5c92
さーな <95469388+serna37@users.noreply.github.com>
2026-01-19 12:39:59 +0900

    テトレーションを追加

--- library/number/mod/mod_tetration.hpp
+++ library/number/mod/mod_tetration.hpp
@@ -1,2 +1,15 @@
 #pragma once
-// TODO
+#include "library/number/euler_phi.hpp"
+#include "library/number/mod/mod_pow.hpp"
+template <typename T>
+T mod_tetration(const T &a, const T &b, const T &m) {
+    if (m == 1) return 0;
+    if (a == 0) return !(b & 1);
+    if (b == 0) return 1;
+    if (b == 1) return a % m;
+    if (b == 2) return mod_pow(a, a, m);
+    auto phi = euler_phi(m);
+    auto tmp = mod_tetration(a, b - 1, phi);
+    if (tmp == 0) tmp += phi;
+    return mod_pow(a, tmp, m);
+}
