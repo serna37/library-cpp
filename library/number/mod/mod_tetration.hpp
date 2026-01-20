5e33e2f7ff09ff91f015d102d664c40418e47d13
さーな <95469388+serna37@users.noreply.github.com>
2026-01-20 09:12:56 +0900

    modify

--- library/number/mod/mod_tetration.hpp
+++ library/number/mod/mod_tetration.hpp
@@ -3,13 +3,13 @@
 #include "library/number/mod/mod_pow.hpp"
 template <typename T>
 T mod_tetration(const T &a, const T &b, const T &m) {
-    if (m == 1) return 0;
-    if (a == 0) return !(b & 1);
-    if (b == 0) return 1;
-    if (b == 1) return a % m;
-    if (b == 2) return mod_pow(a, a, m);
+    if (m == 1ll) return 0ll;
+    if (a == 0ll) return !(b & 1ll);
+    if (b == 0ll) return 1ll;
+    if (b == 1ll) return a % m;
+    if (b == 2ll) return mod_pow(a, a, m);
     auto phi = euler_phi(m);
     auto tmp = mod_tetration(a, b - 1, phi);
-    if (tmp == 0) tmp += phi;
+    if (tmp == 0ll) tmp += phi;
     return mod_pow(a, tmp, m);
 }
