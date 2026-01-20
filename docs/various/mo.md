6ed911defda84056ee2869d476673e210f6434f1
さーな <95469388+serna37@users.noreply.github.com>
2026-01-20 07:04:06 +0000

    ファイルだけ追加

---
+++ docs/various/mo.md
@@ -0,0 +1,18 @@
+---
+title: Mo's algorithm
+documentation_of: //library/various/mo.hpp
+---
+
+# Mo's algorithm
+
+## できること
+- xxx
+
+## 計算量
+$O(N)$
+
+## 使い方
+```cpp
+// XXX
+```
+
---
+++ tests/various.mo.test.cpp
@@ -0,0 +1,9 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/static_range_inversions_query"
+#include "template/template.hpp"
+#include "library/various/mo.hpp"
+/**
+ * @brief Mo's algorithmのテスト
+ */
+void solve() {
+    // TODO
+}
