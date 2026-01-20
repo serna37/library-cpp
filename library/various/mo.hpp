924141549df441f9af5cf73f013dee02f265d44c
さーな <95469388+serna37@users.noreply.github.com>
2026-01-20 16:51:07 +0900

    Mo's Algo追加 (#71)
    * ファイルだけ追加
    
    * Codespace上でのC++実行構成
    
    * modify recommendation plugins
    
    * Mo's Algo追加

---
+++ library/various/mo.hpp
@@ -0,0 +1,54 @@
+#pragma once
+struct Mo {
+  private:
+    int n, w;
+    vector<int> ord;
+    vector<pair<int, int> > lr;
+
+  public:
+    Mo(int n, int q) : n(n), ord(q) {
+        w = max<int>(1ll, n / max(1.0, sqrt(q * 2.0 / 3.0)));
+        iota(ord.begin(), ord.end(), 0ll);
+        lr.reserve(q);
+    }
+    void add_query(int l, int r) {
+        assert(0ll <= l and l < r and r <= n);
+        lr.emplace_back(l, r);
+    }
+    template <typename AL, typename AR, typename EL, typename ER, typename Q>
+    void calclate_queries(const AL &add_left, const AR &add_right,
+        const EL &erase_left, const ER &erase_right,
+        const Q &query) {
+        assert(lr.size() == ord.size());
+        vector<int> bs(n);
+        for (int i = 0, cnt = 0, b = 0; i < n; i++) {
+            bs[i] = b;
+            if (++cnt == w) {
+                ++b;
+                cnt = 0;
+            }
+        }
+        sort(ord.begin(), ord.end(), [&](int a, int b) {
+            int a_block = bs[lr[a].first];
+            int b_block = bs[lr[b].first];
+            if (a_block != b_block) return a_block < b_block;
+            if (a_block & 1) {
+                return lr[a].second < lr[b].second;
+            } else {
+                return lr[a].second > lr[b].second;
+            }
+        });
+        int l = 0, r = 0;
+        for (auto idx : ord) {
+            while (l > lr[idx].first) add_left(--l);
+            while (r < lr[idx].second) add_right(r++);
+            while (l < lr[idx].first) erase_left(l++);
+            while (r > lr[idx].second) erase_right(--r);
+            query(idx);
+        }
+    }
+    template <typename A, typename E, typename Q>
+    void calclate_queries(const A &add, const E &erase, const Q &query) {
+        calclate_queries(add, add, erase, erase, query);
+    }
+};
---
+++ tests/various.mo.test.cpp
@@ -0,0 +1,50 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/static_range_inversions_query"
+#include "template/template.hpp"
+#include "library/sequence/compressor.hpp"
+#include "library/segtree/fenwick_tree.hpp"
+#include "library/various/mo.hpp"
+/**
+ * @brief Mo's algorithmのテスト
+ */
+void solve() {
+    int N, Q;
+    cin >> N >> Q;
+    vector<int> A(N);
+    cin >> A;
+    Mo mo(N, Q);
+    for (int i = 0; i < Q; ++i) {
+        int l, r;
+        cin >> l >> r;
+        mo.add_query(l, r);
+    }
+    Compressor zip(A);
+    A = zip.get_all();
+    FenwickTree fwk(zip.size());
+    int64_t inv = 0, all = 0;
+    vector< int64_t > ans(Q);
+    auto add_left = [&](int idx) {
+        inv += fwk.sum(A[idx] - 1);
+        fwk.add(A[idx], 1);
+        all++;
+    };
+    auto add_right = [&](int idx) {
+        inv += all - fwk.sum(A[idx]);
+        fwk.add(A[idx], 1);
+        ++all;
+    };
+    auto erase_left = [&](int idx) {
+        fwk.add(A[idx], -1);
+        --all;
+        inv -= fwk.sum(A[idx] - 1);
+    };
+    auto erase_right = [&](int idx) {
+        fwk.add(A[idx], -1);
+        --all;
+        inv -= all - fwk.sum(A[idx]);
+    };
+    auto query = [&](int idx) {
+        ans[idx] = inv;
+    };
+    mo.calclate_queries(add_left, add_right, erase_left, erase_right, query);
+    for (auto &p : ans) print(p);
+}
