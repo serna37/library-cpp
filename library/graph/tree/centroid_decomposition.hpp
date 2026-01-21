b2e3542fa01bdfe35415494932169a21663f1f8b
さーな <95469388+serna37@users.noreply.github.com>
2026-01-21 02:30:30 +0000

    グラフのfor修正

--- library/graph/tree/centroid_decomposition.hpp
+++ library/graph/tree/centroid_decomposition.hpp
@@ -18,24 +18,24 @@ struct CentroidDecomposition : Graph {
     vector<int> v;
     inline int build_dfs(int idx, int par) {
         sub[idx] = 1;
-        for (auto &&[from, to, cost, idx] : G[idx]) {
-            if (to == par or v[to]) continue;
-            sub[idx] += build_dfs(to, idx);
+        for (auto &&edge : G[idx]) {
+            if (edge.to == par or v[edge.to]) continue;
+            sub[idx] += build_dfs(edge.to, idx);
         }
         return sub[idx];
     }
     inline int search_centroid(int idx, int par, const int mid) {
-        for (auto &&[from, to, cost, idx] : G[idx]) {
-            if (to == par or v[to]) continue;
-            if (sub[to] > mid) return search_centroid(to, idx, mid);
+        for (auto &&edge : G[idx]) {
+            if (edge.to == par or v[edge.to]) continue;
+            if (sub[edge.to] > mid) return search_centroid(edge.to, idx, mid);
         }
         return idx;
     }
     inline int build_dfs(int idx) {
         int centroid = search_centroid(idx, -1, build_dfs(idx, -1) / 2ll);
         v[centroid] = true;
-        for (auto &&[from, to, cost, idx] : G[centroid]) {
-            if (!v[to]) tree.add(centroid, build_dfs(to));
+        for (auto &&edge : G[centroid]) {
+            if (!v[edge.to]) tree.add(centroid, build_dfs(edge.to));
         }
         v[centroid] = false;
         return centroid;
