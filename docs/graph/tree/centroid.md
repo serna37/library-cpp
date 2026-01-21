ab2eed40e8a28a30e43a021e9465f67b453b2825
さーな <95469388+serna37@users.noreply.github.com>
2026-01-21 00:35:23 +0000

    木の重心を追加

---
+++ docs/graph/tree/centroid.md
@@ -0,0 +1,22 @@
+---
+title: 木の重心
+documentation_of: //library/tree/centroid.hpp
+---
+
+# 木の重心
+
+## できること
+- 木の重心を求める
+- 重心 $\cdots$ その頂点を取り除いたときにできる部分木たちの頂点数が、全て半分以下になる
+- 重心はは1個か2個存在する
+
+## 計算量
+$O(V)$
+
+## 使い方
+```cpp
+int N;
+Graph G(N);
+vector<int> C = centroid(G);
+```
+
---
+++ library/graph/tree/centroid.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#include "library/graph/base/graph.hpp"
+template <typename T> vector<int> centroid(const Graph<T> &G) {
+    const int N = (int)G.size();
+    stack<pair<int, int>> st;
+    st.emplace(0ll, -1ll);
+    vector<int> sz(N), par(N);
+    while (!st.empty()) {
+        auto p = st.top();
+        if (sz[p.first] == 0ll) {
+            sz[p.first] = 1;
+            for (auto &to : G[p.first]) {
+                if (to != p.second) st.emplace(to, p.first);
+            }
+        } else {
+            for (auto &to : G[p.first]) {
+                if (to != p.second) sz[p.first] += sz[to];
+            }
+            par[p.first] = p.second;
+            st.pop();
+        }
+    }
+    vector<int> ret;
+    int size = N;
+    for (int i = 0; i < N; ++i) {
+        int val = N - sz[i];
+        for (auto &to : G[i]) {
+            if (to != par[i]) val = max(val, sz[to]);
+        }
+        if (val < size) size = val, ret.clear();
+        if (val == size) ret.emplace_back(i);
+    }
+    return ret;
+}
