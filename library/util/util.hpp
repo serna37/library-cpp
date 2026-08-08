1f942400219df5f93c67f956d9abc57c61f687a4
さーな <95469388+serna37@users.noreply.github.com>
2026-08-08 21:09:40 +0900

    add util hpp

---
+++ library/util/util.hpp
@@ -0,0 +1,7 @@
+/** ----- start include utility ----- */
+#include "library/sequence/collection_util.hpp"
+#include "library/string/util.hpp"
+#include "library/geometry/util.hpp"
+#include "library/geometry/area_square.hpp"
+#include "library/geometry/area_triangle.hpp"
+/** ----- end include utility ----- */
