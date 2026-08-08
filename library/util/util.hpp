e8d0092e6d59013e7fc6c42afcc6e12be1a8d593
さーな <95469388+serna37@users.noreply.github.com>
2026-08-08 21:18:04 +0900

    modify util hpp

--- library/util/util.hpp
+++ library/util/util.hpp
@@ -1,7 +1,16 @@
 /** ----- start include utility ----- */
 #include "library/sequence/collection_util.hpp"
 #include "library/string/util.hpp"
+#include "library/string/run_length.hpp"
 #include "library/geometry/util.hpp"
 #include "library/geometry/area_square.hpp"
 #include "library/geometry/area_triangle.hpp"
+#include "library/number/div.hpp"
+#include "library/number/divisors.hpp"
+#include "library/number/ext_gcd.hpp"
+#include "library/number/factorial.hpp"
+#include "library/number/combination.hpp"
+#include "library/number/base_convert.hpp"
+#include "library/number/prime/prime_fact.hpp"
+#include "library/number/prime/prime_test.hpp"
 /** ----- end include utility ----- */
