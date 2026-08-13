eefc4484820fcdb974d1100b51293883d703ea3f
さーな <95469388+serna37@users.noreply.github.com>
2026-08-13 14:02:15 +0900

    modify

--- core/util.hpp
+++ core/util.hpp
@@ -1,4 +1,5 @@
-#pragma once
+#include "template/template.hpp"
+
 /** ----- start include utility ----- */
 #include "library/sequence/collection_util.hpp"
 #include "library/string/util.hpp"
@@ -15,3 +16,18 @@
 #include "library/number/prime/prime_fact.hpp"
 #include "library/number/prime/prime_test.hpp"
 /** ----- end include utility ----- */
+
+/** ===================================== */
+/** START include precious library */
+#include "library/number/mod/montgomery_mod_int.hpp"
+#include "library/number/prime/eratosthenes.hpp"
+#include "library/string/rolling_hash.hpp"
+#include "library/string/aho_corasick.hpp"
+#include "library/graph/connected_components/strongly_connected_components.hpp"
+#include "library/graph/tree/heavy_light_decomposition.hpp"
+#include "library/graph/tree/centroid_decomposition.hpp"
+#include "library/segtree/lazy_segment_tree.hpp"
+#include "library/sequence/wavelet_matrix.hpp"
+#include "library/polynomial/fps/stirling_second_number.hpp"
+/** DONE */
+/** ===================================== */
